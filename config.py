e03ddde7f4923cddf29f90b97890d152ffa3b8be
Romayne <27376947+MeggalBozale@users.noreply.github.com>
2024-09-12 19:32:56 -0400

    shorten config variable names and name alphabetical+lowercase only

--- config.py
+++ config.py
@@ -17,19 +17,19 @@ _public_config:dict = utils.read_file_json(config_paths.FILE_JSON_PUBLIC_CONFIG)
 
 # Setup default values
 _default_config = {
-    "do_difficulty_scale"   : True,
-    "show_fish"             : True,
-    "fishing_level"         : 10,
-    "season"                : constants.SEASON_SPRING,
-    "weather"               : constants.WEATHER_SUNNY,
-    "time"                  : utils.classic_to_military("6:00AM"),
-    "scale_perfect_catches" : 1,
-    "rod_used"              : constants.FISHING_ROD_IRIDIUM,
-    "bait_used"             : constants.FISHING_BAIT_NONE,
-    "lure_used"             : constants.FISHING_LURE_NONE,
-    "bait_target_id"        : None,
-    "water_depth"           : 5,
-    "locations"             : [
+    "scaleperfect" : True,
+    "showfish"     : True,
+    "fishinglevel" : 10,
+    "season"       : constants.SEASON_SPRING,
+    "weather"      : constants.WEATHER_SUNNY,
+    "time"         : utils.classic_to_military("6:00AM"),
+    "pctperfect"   : 1,
+    "rod"          : constants.FISHING_ROD_IRIDIUM,
+    "bait"         : constants.FISHING_BAIT_NONE,
+    "lure"         : constants.FISHING_LURE_NONE,
+    "baitid"       : None,
+    "depth"        : 5,
+    "locations"    : [
     constants.LOCATION_BEACH,
     constants.LOCATION_FOREST,
     constants.LOCATION_TOWN,
@@ -43,18 +43,18 @@ for config_key in _default_config.keys():
         _default_config[config_key] = _public_config[config_key]
 
 # Take values from default config and put them into variables for easy use elsewhere.
-DO_PERFECTION_DIFFICULTY_SCALE = _default_config["do_difficulty_scale"] # enable scaling fish perfection % based off factors like its difficulty
+DO_PERFECTION_DIFFICULTY_SCALE = _default_config["scaleperfect"] # enable scaling fish perfection % based off factors like its difficulty
 
-SHOW_FISH     = _default_config["show_fish"] # whether to show individual catchable info for each location
-FISHING_LEVEL = _default_config["fishing_level"]
+SHOW_FISH     = _default_config["showfish"] # whether to show individual catchable info for each location
+FISHING_LEVEL = _default_config["fishinglevel"]
 SEASON        = _default_config["season"]
 WEATHER       = _default_config["weather"]
 TIME          = _default_config["time"]
 LOCATIONS     = _default_config["locations"]
 
-SCALE_PCT_PERFECT_CATCHES = _default_config["scale_perfect_catches"] # the percentage of catches which will be perfect. (0-1)
-ROD_USED                  = _default_config["rod_used"]
-BAIT_USED                 = _default_config["bait_used"]
-LURE_USED                 = _default_config["lure_used"]
-BAIT_TARGET_ID            = _default_config["bait_target_id"] # (sardine) the current object ID of the targeted bait (if any) being used.
-WATER_DEPTH               = _default_config["water_depth"] # fishing zone (0, 1, 2, 3, 5)
+SCALE_PCT_PERFECT_CATCHES = _default_config["pctperfect"] # the percentage of catches which will be perfect. (0-1)
+ROD_USED                  = _default_config["rod"]
+BAIT_USED                 = _default_config["bait"]
+LURE_USED                 = _default_config["lure"]
+BAIT_TARGET_ID            = _default_config["baitid"] # (sardine) the current object ID of the targeted bait (if any) being used.
+WATER_DEPTH               = _default_config["depth"] # fishing zone (0, 1, 2, 3, 5)
